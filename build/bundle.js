3803966d5392e3e6bc5609e10bc2b99b5053a840
Colin <colingaojing@163.com>
2020-02-14 14:53:36 +0800

    服务端渲染路由引入

--- build/bundle.js
+++ build/bundle.js
@@ -86,6 +86,18 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/Routes.js":
+/*!***********************!*\
+  !*** ./src/Routes.js ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n    value: true\n});\n\nvar _react = __webpack_require__(/*! react */ \"react\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactRouterDom = __webpack_require__(/*! react-router-dom */ \"react-router-dom\");\n\nvar _Home = __webpack_require__(/*! ./containers/Home */ \"./src/containers/Home/index.js\");\n\nvar _Home2 = _interopRequireDefault(_Home);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\nexports.default = _react2.default.createElement(\n    'div',\n    null,\n    _react2.default.createElement(_reactRouterDom.Route, { path: '/', exact: true, component: _Home2.default })\n);\n\n//# sourceURL=webpack:///./src/Routes.js?");
+
+/***/ }),
+
 /***/ "./src/containers/Home/index.js":
 /*!**************************************!*\
   !*** ./src/containers/Home/index.js ***!
@@ -106,7 +118,7 @@ eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n    value: true\n});
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\nvar _express = __webpack_require__(/*! express */ \"express\");\n\nvar _express2 = _interopRequireDefault(_express);\n\nvar _Home = __webpack_require__(/*! ../containers/Home */ \"./src/containers/Home/index.js\");\n\nvar _Home2 = _interopRequireDefault(_Home);\n\nvar _react = __webpack_require__(/*! react */ \"react\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _server = __webpack_require__(/*! react-dom/server */ \"react-dom/server\");\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n// const express = require('express')\n// const Home = require('./containers/Home/index')\n// 用了webpack之后，可以使用es-module的方式引入\nvar app = (0, _express2.default)();\n// 如果访问静态文件，比如<script src='/index.js'></script> 就去根目录的public下面去找\napp.use(_express2.default.static('public'));\n\nvar content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));\n\napp.get('/', function (req, res) {\n    return (\n        // 把Home组件渲染成字符串返回给浏览器\n        res.send('\\n        <html>\\n            <head>\\n                <title>SSR</title>\\n            </head>\\n            <body>\\n                <div id=\"root\">' + content + '</div>\\n            </body>\\n            <script src=\\'/index.js\\'></script>\\n        </html>\\n    ')\n    );\n});\n\napp.listen(3000, function () {\n    return console.log('Example app listening on port 3000!');\n});\n\n//# sourceURL=webpack:///./src/server/index.js?");
+eval("\n\nvar _express = __webpack_require__(/*! express */ \"express\");\n\nvar _express2 = _interopRequireDefault(_express);\n\nvar _react = __webpack_require__(/*! react */ \"react\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _server = __webpack_require__(/*! react-dom/server */ \"react-dom/server\");\n\nvar _reactRouterDom = __webpack_require__(/*! react-router-dom */ \"react-router-dom\");\n\nvar _Routes = __webpack_require__(/*! ../Routes */ \"./src/Routes.js\");\n\nvar _Routes2 = _interopRequireDefault(_Routes);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n// 服务端渲染路由需要使用StaticRouter\nvar app = (0, _express2.default)();\n// 如果访问静态文件，比如<script src='/index.js'></script> 就去根目录的public下面去找\n// const express = require('express')\n// const Home = require('./containers/Home/index')\n// 用了webpack之后，可以使用es-module的方式引入\napp.use(_express2.default.static('public'));\n\napp.get('/', function (req, res) {\n    var content = (0, _server.renderToString)(\n    // 服务端渲染需要获取浏览器的访问路径req.path\n    _react2.default.createElement(\n        _reactRouterDom.StaticRouter,\n        { context: {}, location: req.path },\n        _Routes2.default\n    ));\n    // 把Home组件渲染成字符串返 回给浏览器\n    res.send('\\n            <html>\\n                <head>\\n                    <title>SSR</title>\\n                </head>\\n                <body>\\n                    <div id=\"root\">' + content + '</div>\\n                </body>\\n                <script src=\\'/index.js\\'></script>\\n            </html>\\n        ');\n});\n\napp.listen(3000, function () {\n    return console.log('Example app listening on port 3000!');\n});\n\n//# sourceURL=webpack:///./src/server/index.js?");
 
 /***/ }),
 
@@ -140,6 +152,17 @@ eval("module.exports = require(\"react\");\n\n//# sourceURL=webpack:///external_
 /***/ (function(module, exports) {
 
 eval("module.exports = require(\"react-dom/server\");\n\n//# sourceURL=webpack:///external_%22react-dom/server%22?");
+
+/***/ }),
+
+/***/ "react-router-dom":
+/*!***********************************!*\
+  !*** external "react-router-dom" ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"react-router-dom\");\n\n//# sourceURL=webpack:///external_%22react-router-dom%22?");
 
 /***/ })
 
--- src/server/index.js
+++ src/server/index.js
@@ -2,29 +2,36 @@
 // const Home = require('./containers/Home/index')
 // 用了webpack之后，可以使用es-module的方式引入
 import express from 'express'
-import Home from '../containers/Home'
 import React from 'react'
 import {renderToString} from 'react-dom/server'
+// 服务端渲染路由需要使用StaticRouter
+import {StaticRouter} from 'react-router-dom'
+import Routes from '../Routes'
 
 const app = express()
 // 如果访问静态文件，比如<script src='/index.js'></script> 就去根目录的public下面去找
 app.use(express.static('public'))
 
-const content = renderToString(<Home/>)
-
-app.get('/', (req, res) => 
-    // 把Home组件渲染成字符串返回给浏览器
-    res.send(`
-        <html>
-            <head>
-                <title>SSR</title>
-            </head>
-            <body>
-                <div id="root">${content}</div>
-            </body>
-            <script src='/index.js'></script>
-        </html>
-    `)
+app.get('/', (req, res) => {
+        const content = renderToString((
+            // 服务端渲染需要获取浏览器的访问路径req.path
+            <StaticRouter context={{}} location={req.path}>
+                {Routes}
+            </StaticRouter>
+        ))
+        // 把Home组件渲染成字符串返 回给浏览器
+        res.send(`
+            <html>
+                <head>
+                    <title>SSR</title>
+                </head>
+                <body>
+                    <div id="root">${content}</div>
+                </body>
+                <script src='/index.js'></script>
+            </html>
+        `)
+    }
 )
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
