a003ffd27c801a66df2aa0f886e3a30d751dd42d
Colin <colingaojing@163.com>
2020-02-14 21:33:43 +0800

    引入redux

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,12 +2,13 @@
 // const Reat = require('react')
 import React from 'react';
 import Header from '../../components/Header'
+import {connect} from 'react-redux'
 
-const Home = () => {
+const Home = (props) => {
     return (
         <div>
             <Header/>
-            <div>Home</div>
+            <div>Home,this is {props.name}</div>
             {/* 
                 renderToString只能渲染基础结构，无法渲染事件绑定————引入同构
                 若想事件正常执行，Home组件的代码先在服务端执行一次，然后在客户端也要执行一次
@@ -22,4 +23,7 @@ const Home = () => {
 // module.exports = {
 //     default: Home
 // }
-export default Home
+const mapStateToProps = state => ({
+    name: state.name
+})
+export default connect(mapStateToProps, null)(Home)
--- src/server/utils.js
+++ src/server/utils.js
@@ -3,13 +3,23 @@ import {renderToString} from 'react-dom/server'
 // 服务端渲染路由需要使用StaticRouter
 import {StaticRouter} from 'react-router-dom'
 import Routes from '../Routes'
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 
 export const render = (req) => {
+    const reducer = (state = {name:'colin'}, action) => {
+        return state
+    }
+    const store = createStore(reducer, applyMiddleware(thunk))
     const content = renderToString((
-        // 服务端渲染需要获取浏览器的访问路径req.path
-        <StaticRouter context={{}} location={req.path}>
-            {Routes}
-        </StaticRouter>
+        <Provider store={store}>
+            {/* 服务端渲染需要获取浏览器的访问路径req.path */}
+            <StaticRouter context={{}} location={req.path}>
+                {Routes}
+            </StaticRouter>
+        </Provider>
+        
     ))
     return (
         `
